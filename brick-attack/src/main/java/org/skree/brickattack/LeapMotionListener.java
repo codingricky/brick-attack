2e7cb1c09e3da90f8521a2d110cc680c45fc8b3c
Ricky <ricky@dius.com.au>
2013-12-24 16:31:00 +1100

    got leapmotion working

---
+++ brick-attack/src/main/java/org/skree/brickattack/LeapMotionListener.java
@@ -0,0 +1,27 @@
+package org.skree.brickattack;
+
+
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Hand;
+import com.leapmotion.leap.Listener;
+
+public class LeapMotionListener extends Listener {
+
+    private float x;
+
+    public LeapMotionListener() {
+    }
+
+    public void onFrame(Controller controller) {
+        final Hand hand = controller.frame().hands().get(0);
+        if (hand.palmPosition().isValid()) {
+            x = hand.palmPosition().getX();
+        } else {
+            x = 0;
+        }
+    }
+
+    public float getX() {
+        return x;
+    }
+}
