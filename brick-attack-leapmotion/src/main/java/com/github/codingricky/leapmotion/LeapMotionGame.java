443a9c0f5abc4e873e97c4cba5cb74f0babee874
Ricky <ricky@dius.com.au>
2013-12-24 22:28:36 +1100

    extracted constant

--- brick-attack-leapmotion/src/main/java/com/github/codingricky/leapmotion/LeapMotionGame.java
+++ brick-attack-leapmotion/src/main/java/com/github/codingricky/leapmotion/LeapMotionGame.java
@@ -6,17 +6,20 @@ import org.skree.brickattack.PaddleEntity;
 
 public class LeapMotionGame extends Game {
 
+    private static final int MOVEMENT_MULTIPLIER = 4;
     private final Controller controller;
     private final LeapMotionListener listener;
 
     public LeapMotionGame() {
+        // need to keep references to these objects otherwise
+        // the leap motion libraries crash
         listener = new LeapMotionListener();
         controller = new Controller(listener);
     }
 
     @Override
     protected void movePaddle(PaddleEntity paddle) {
-        paddle.setHorizontalSpeed(listener.getX() * 4);
+        paddle.setHorizontalSpeed(listener.getX() * MOVEMENT_MULTIPLIER);
     }
 
     public static void main(String[] args) {
